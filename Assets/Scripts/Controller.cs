71453a287c4b5302cc566248153cab01945890dc
0101pablo <0101pablo@gmail.com>
2017-09-07 17:10:09 -0300

    Começo do proejto utilizando GitHub

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+
+    public GameObject go;
+    private Vector3 vec;
+    public int controleObjeto;
+    private float posicao = 0.2f;
+
+    private void Start()
+    {
+
+        PlayerPrefs.SetInt("posVert", 1);
+        PlayerPrefs.SetInt("posHori", 1);
+
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        if(controleObjeto == 1) { 
+        
+            switch (PlayerPrefs.GetInt("posVert"))
+            {
+
+                case 1:
+
+                    vec = new Vector3(go.transform.position.x + posicao * Time.deltaTime, go.transform.position.y, go.transform.position.z);
+                    go.transform.SetPositionAndRotation(vec, Quaternion.identity);
+                    print("indo = " + posicao);
+                    if (posicao <= 0.5f)
+                    {
+
+                        PlayerPrefs.SetInt("posVert", 2);
+
+                    }
+                    else
+                    {
+
+                        posicao = posicao - 0.1f;
+
+                    }
+
+                    break;
+
+                case 2:
+
+
+
+                    vec = new Vector3(go.transform.position.x - posicao * Time.deltaTime, go.transform.position.y, go.transform.position.z);
+                    go.transform.SetPositionAndRotation(vec, Quaternion.identity);
+                    print("vindo = " + posicao);
+                    if (posicao >= 2f)
+                    {
+                        PlayerPrefs.SetInt("posVert", 1);
+
+                    }
+                    else
+                    {
+
+                        posicao = posicao + 0.1f;
+                    }
+               
+                
+
+                    break;
+
+            }
+        }else if(controleObjeto == 2)
+        {
+            switch (PlayerPrefs.GetInt("posHori"))
+            {
+
+                case 1:
+
+                    vec = new Vector3(go.transform.position.x, go.transform.position.y + posicao * Time.deltaTime, go.transform.position.z);
+                    go.transform.SetPositionAndRotation(vec, Quaternion.identity);
+                    print("indo = " + posicao);
+                    if (posicao <= 0.5f)
+                    {
+
+                        PlayerPrefs.SetInt("posHori", 2);
+
+                    }
+                    else
+                    {
+
+                        posicao = posicao - 0.1f;
+
+                    }
+
+                    break;
+
+                case 2:
+
+
+
+                    vec = new Vector3(go.transform.position.x, go.transform.position.y - posicao * Time.deltaTime, go.transform.position.z);
+                    go.transform.SetPositionAndRotation(vec, Quaternion.identity);
+                    print("vindo = " + posicao);
+                    if (posicao >= 2f)
+                    {
+                        PlayerPrefs.SetInt("posHori", 1);
+
+                    }
+                    else
+                    {
+
+                        posicao = posicao + 0.1f;
+                    }
+
+
+
+                    break;
+
+            }
+
+
+
+        }
+
+
+
+
+
+    }
+        
+
+}
---
+++ Assets/Scripts/Projetil1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil1 : MonoBehaviour {
+
+    int toque = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (toque >= 4)
+        {
+            Destroy(gameObject);
+        }
+        else toque++;
+    }
+
+}
