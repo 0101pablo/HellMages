5b98bb4b7b28952f01b2bb52039c8883be816142
0101pablo <0101pablo@gmail.com>
2017-09-08 03:58:39 -0300

    movimentacao player1

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,130 +4,68 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
 
-
     public GameObject go;
     private Vector3 vec;
     public int controleObjeto;
     private float posicao = 0.2f;
 
-    private void Start()
-    {
-
+    private void Start() {
         PlayerPrefs.SetInt("posVert", 1);
         PlayerPrefs.SetInt("posHori", 1);
-
     }
 
-
-    private void FixedUpdate()
-    {
-
+    private void FixedUpdate() {
         if(controleObjeto == 1) { 
-        
-            switch (PlayerPrefs.GetInt("posVert"))
-            {
-
+            switch (PlayerPrefs.GetInt("posVert")) {
                 case 1:
-
                     vec = new Vector3(go.transform.position.x + posicao * Time.deltaTime, go.transform.position.y, go.transform.position.z);
                     go.transform.SetPositionAndRotation(vec, Quaternion.identity);
-                    print("indo = " + posicao);
-                    if (posicao <= 0.5f)
-                    {
-
+                    //print("indo = " + posicao);
+                    if (posicao <= 0.5f) {
                         PlayerPrefs.SetInt("posVert", 2);
-
                     }
-                    else
-                    {
-
+                    else {
                         posicao = posicao - 0.1f;
-
                     }
-
                     break;
-
                 case 2:
-
-
-
                     vec = new Vector3(go.transform.position.x - posicao * Time.deltaTime, go.transform.position.y, go.transform.position.z);
                     go.transform.SetPositionAndRotation(vec, Quaternion.identity);
-                    print("vindo = " + posicao);
-                    if (posicao >= 2f)
-                    {
+                    //print("vindo = " + posicao);
+                    if (posicao >= 2f) {
                         PlayerPrefs.SetInt("posVert", 1);
-
                     }
-                    else
-                    {
-
+                    else {
                         posicao = posicao + 0.1f;
                     }
-               
-                
-
                     break;
-
             }
-        }else if(controleObjeto == 2)
-        {
-            switch (PlayerPrefs.GetInt("posHori"))
-            {
-
+        }
+        else if(controleObjeto == 2) {
+            switch (PlayerPrefs.GetInt("posHori")) {
                 case 1:
-
                     vec = new Vector3(go.transform.position.x, go.transform.position.y + posicao * Time.deltaTime, go.transform.position.z);
                     go.transform.SetPositionAndRotation(vec, Quaternion.identity);
-                    print("indo = " + posicao);
-                    if (posicao <= 0.5f)
-                    {
-
+                    //print("indo = " + posicao);
+                    if (posicao <= 0.5f) {
                         PlayerPrefs.SetInt("posHori", 2);
-
                     }
-                    else
-                    {
-
+                    else {
                         posicao = posicao - 0.1f;
-
                     }
-
                     break;
-
                 case 2:
-
-
-
                     vec = new Vector3(go.transform.position.x, go.transform.position.y - posicao * Time.deltaTime, go.transform.position.z);
                     go.transform.SetPositionAndRotation(vec, Quaternion.identity);
-                    print("vindo = " + posicao);
-                    if (posicao >= 2f)
-                    {
+                    //print("vindo = " + posicao);
+                    if (posicao >= 2f) {
                         PlayerPrefs.SetInt("posHori", 1);
-
                     }
-                    else
-                    {
-
+                    else {
                         posicao = posicao + 0.1f;
                     }
-
-
-
                     break;
-
             }
-
-
-
         }
-
-
-
-
-
     }
-        
-
 }
---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+    Rigidbody2D rb2d = new Rigidbody2D();
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movimentacao();
+	}
+
+    void Movimentacao()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * 1f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.right * 1f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //transform.Translate(Vector2.up * 100f * Time.deltaTime);
+            rb2d.AddForce(transform.up * 200);
+        }
+    }
+}
