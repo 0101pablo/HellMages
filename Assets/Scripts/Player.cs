f4b325749b4fb393eaf8306bd2321329c5441a37
0101pablo <0101pablo@gmail.com>
2017-09-10 21:45:29 -0300

    Força e ângulo do Tiro (em andamento)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -78,10 +78,10 @@ public class Player : MonoBehaviour {
         var rotacao = directionBar.transform.rotation.eulerAngles;
         if (Input.GetKeyUp(KeyCode.Space) && barrasIniciadas && !powerBarSwitch) directionBarSwitch = true;
         if (directionBarSwitch){
-            if (rotacao.z <=10f) barMove = true;
-            if (rotacao.z >= 170f) barMove = false;
-            if (barMove) rotacao.z = rotacao.z +2;
-            else if (!barMove) rotacao.z = rotacao.z -2;
+            if (rotacao.z <= 10f) barMove = true;
+            if (rotacao.z >= 90f) barMove = false;
+            if (barMove) rotacao.z = rotacao.z + 2f;
+            else if (!barMove) rotacao.z = rotacao.z - 2f;
             directionBar.transform.rotation = Quaternion.Euler(rotacao);
             if (Input.GetKeyDown(KeyCode.Space)){
                 directionBarSwitch = false;
@@ -104,9 +104,11 @@ public class Player : MonoBehaviour {
         GameObject projetilAClone = Instantiate(projetilAPrefab, saidaProjetil.transform.position,Quaternion.Euler(rotacao)) as GameObject;
         Rigidbody2D rbProjA = projetilAClone.GetComponent<Rigidbody2D>();
         rbProjA.transform.rotation = Quaternion.Euler(rotacao);
+        rbProjA.velocity = new Vector2(powerBarValue * (90f / rotacao.z) * 2, (rotacao.z / 90f) * powerBarValue * 10) * 2;
+        print(rotacao.z);
 
         //Estudar como arremessar no ângulo certo
-        rbProjA.AddForce(transform.right * powerBarValue * 1000); //a força está OK, porém não está obedecendo o ângulo. Estudar
+        //rbProjA.AddForce(transform.right * powerBarValue * 1000); //a força está OK, porém não está obedecendo o ângulo. Estudar
 
         TrocaPlayers();
     }
